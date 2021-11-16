7cedbde49078a9681b51253ae9b14d43b549bd30
Alberto <albertoagamez@gmail.com>
2021-11-15 21:26:42 -0500

    resuelto 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -215,7 +215,7 @@ return ((base*altura)/2)
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
-  //como parámetro un número de euros y calcule el cambio en dólares.
+  //como parámetro un número de euros y calcule el cambio en dólares..
   //Escribe tu código aquí
   return 1.2*euro
 }
